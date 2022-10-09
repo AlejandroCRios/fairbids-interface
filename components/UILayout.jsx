8351af187a67da5d4758ff2ca913e00140c34176
AlejandroCRios <al.cardonarios@gmail.com>
2022-10-09 12:33:10 -0500

    head updated

--- components/UILayout.jsx
+++ components/UILayout.jsx
@@ -18,17 +18,17 @@ const Layout = ({ children }) => {
     >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="VMax" />
-        <meta name="author" content="VMax" />
-        <meta name="author" content="VMax" />
+        <meta name="description" content="FairBid" />
+        <meta name="author" content="FairBid" />
+        <meta name="author" content="FairBid" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="VMax" />
-        <meta property="og:site_name" content="VMax" />
-        <meta name="og:title" content="VMax" />
+        <meta name="twitter:title" content="FairBid" />
+        <meta property="og:site_name" content="FairBid" />
+        <meta name="og:title" content="FairBid" />
         <meta property="og:type" content="website" />
 
-        <title>VMax - interface</title>
+        <title>FairBid - interface</title>
       </Head>
       <TokenModal />
       <Navbar></Navbar>
